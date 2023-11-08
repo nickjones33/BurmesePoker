253c13814bce885e045f2827f81c199dcf70bcc1
Nicholas Jones <nickjones33@gmail.com>
2023-11-08 13:17:04 -0600

    messed up my ignore

--- BurmesePoker/obj/Debug/net8.0/BurmesePoker.AssemblyInfo.cs
+++ BurmesePoker/obj/Debug/net8.0/BurmesePoker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BurmesePoker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d1c7087c6e751196070b622c2c5ee522457e90")]
 [assembly: System.Reflection.AssemblyProductAttribute("BurmesePoker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BurmesePoker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
